faa143d151341be8ca03cd43bc4e840b3a4b130c
JP-sDEV <jon.pablo80@gmail.com>
2024-07-24 17:06:22 -0400

    feat: integrate s3 storage for save and get methods

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,11 +1,3 @@
 // src/model/data/index.js
 
-const logger = require('../../logger');
-
-if (process.NODE_ENV !== 'production') {
-    logger.info('using in memory db');
-    module.exports = require('./memory');
-} else {
-    // Amazon Service
-    logger.info('using Amazon db');
-}
+module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
