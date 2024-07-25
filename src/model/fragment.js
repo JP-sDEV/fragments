61b98303d79004f830452aef8042eb4fdd538a5c
JP-sDEV <jon.pablo80@gmail.com>
2024-07-25 12:45:59 -0400

    feat: switch between local and aws configs

--- src/model/fragment.js
+++ src/model/fragment.js
@@ -12,7 +12,7 @@ const {
     writeFragmentData,
     listFragments,
     deleteFragment,
-} = require('./data/memory');
+} = require('./data');
 const logger = require('../logger');
 
 class Fragment {
