c5f6589d3f16f0fb188cab89e0c92115fdb286ed
JP-sDEV <jon.pablo80@gmail.com>
2024-07-25 11:07:57 -0400

    feat: add DELETE fragment

--- src/model/data/aws/index.js
+++ src/model/data/aws/index.js
@@ -4,10 +4,10 @@
 const MemoryDB = require('../memory/memory-db');
 const s3Client = require('./s3Client');
 const logger = require('../../../logger');
-const { PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { PutObjectCommand, GetObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
 
 // Create two in-memory databases: one for fragment metadata and the other for raw data
-const data = new MemoryDB();
+// const data = new MemoryDB();
 const metadata = new MemoryDB();
 
 // Write a fragment's metadata to memory db. Returns a Promise
@@ -103,14 +103,36 @@ async function listFragments(ownerId, expand = false) {
     return fragments.map((fragment) => fragment.id);
 }
 
+// // Delete a fragment's metadata and data from memory db. Returns a Promise
+// function deleteFragment(ownerId, id) {
+//     return Promise.all([
+//         // Delete metadata
+//         metadata.del(ownerId, id),
+//         // Delete data
+//         data.del(ownerId, id),
+//     ]);
+// }
+
 // Delete a fragment's metadata and data from memory db. Returns a Promise
-function deleteFragment(ownerId, id) {
-    return Promise.all([
-        // Delete metadata
-        metadata.del(ownerId, id),
-        // Delete data
-        data.del(ownerId, id),
-    ]);
+async function deleteFragment(ownerId, id) {
+    const params = new DeleteObjectCommand({
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: `${ownerId}/${id}`,
+    });
+
+    const command = new DeleteObjectCommand(params);
+
+    try {
+        // delete metadata
+        await metadata.del(ownerId, id);
+        // delete fragment data
+        await s3Client.sned(command);
+    } catch (err) {
+        logger.error(
+            { err, Bucket: params.Bucket, Key: params.Key },
+            'Error deleting fragment data from S3'
+        );
+    }
 }
 
 module.exports.listFragments = listFragments;
