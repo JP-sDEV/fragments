911414c64def53239518fb68bda29e0d4e132707
JP-sDEV <jon.pablo80@gmail.com>
2024-07-28 22:35:59 -0400

    chore: add logging

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -10,45 +10,45 @@ module.exports = async (req, res) => {
     logger.info({ req }, '/POST');
 
     try {
+        // Check if request body is a Buffer
         if (!Buffer.isBuffer(req.body)) {
-            logger.warn({ req }, 'Unsupported Content Types');
+            logger.warn(
+                { reqBody: req.body, contentType: req.headers['content-type'] },
+                'Unsupported Content Types'
+            );
             return res.status(415).json(createErrorResponse(415, 'Unsupported Content Type'));
         }
 
-        logger.info({ API_URL: process.env.API_URL }, 'API_URL .env');
-
-        // Create fragment if type is supported
-        const fragmentData = req.body;
         const { type } = contentType.parse(req.headers['content-type']);
 
-        logger.info({ type: type }, 'Content type header');
+        logger.info({ type }, 'Content type header parsed');
 
+        // Create a new Fragment
         let newFragment = new Fragment({
             ownerId: req.user,
             type: type,
         });
 
-        logger.info({ newFragment: newFragment }, 'Fragment Data');
+        logger.info({ newFragment: newFragment }, 'Fragment Data created');
 
         // Save fragment meta data
         await newFragment.save();
         logger.info({ newFragmentId: newFragment.id }, 'Fragment ID after save');
 
         // Save fragment content
-        await newFragment.setData(fragmentData);
+        await newFragment.setData(req.body);
 
+        // Generate URL for the new fragment
         const location = new URL(`/v1/fragments/${newFragment.id}`, process.env.API_URL);
 
-        // const apiUrl = process.env.API_URL || `${req.protocol}://${req.headers.host}`; // ISSUE: process.env.API_URL is undefined
-        // const location = new URL(`/v1/fragments/${newFragment.id}`, apiUrl);
-
+        // Set Location header in response
         res.setHeader('Location', location.toString());
 
-        logger.info({ location: location }, 'Fragment Location: ');
-        logger.info({ res }, '/POST response success');
+        logger.info({ location: location.toString() }, 'Fragment Location set');
+        logger.info({ resStatus: 201, newFragmentId: newFragment.id }, '/POST response success');
         res.status(201).json(createSuccessResponse({ fragment: newFragment }));
     } catch (err) {
-        logger.error({ err, req }, '422: Cannot process type');
+        logger.error({ err, req, stack: err.stack }, '422: Cannot process request');
         res.status(422).json(createErrorResponse(422, 'Cannot process request'));
     }
 };
