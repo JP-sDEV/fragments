be15f3365190fe3c77562956cf4adcdc85f95d00
JP-sDEV <jon.pablo80@gmail.com>
2024-08-03 13:14:07 -0400

    fix: corrected data strategy

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,3 +1,3 @@
 // src/model/data/index.js
 
-module.exports = process.env.AWS_ACCESS_KEY_ID == 'test' ? require('./aws') : require('./memory');
+module.exports = process.env.AWS_ACCESS_KEY_ID === 'test' ? require('./memory') : require('./aws');
